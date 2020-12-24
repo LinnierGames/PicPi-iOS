b5715cc0be6b68f5bbee37d4ccec86fde7ebb25c
Mustafa <dev.mustafa.t.mohammed@gmail.com>
2020-12-23 22:32:36 -0500

    -

--- PicPi/PhotosUpload.swift
+++ PicPi/PhotosUpload.swift
@@ -14,7 +14,6 @@ enum PhotosUpload {
   case retrieveThumbnails
   case removePhoto(filename: String)
  }
-
 extension PhotosUpload: TargetType {
   var baseURL: URL {
      ///Pi IP hard coded for now
