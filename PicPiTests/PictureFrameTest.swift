1f87eb806851b7b444358f8c5a4b8394c2aed605
RawiSoft <dev.mustafa.t.mohammed@gmail.com>
2021-01-17 09:22:57 -0500

    test setPref promise reject with frameAPIErrors

--- PicPiTests/PictureFrameTest.swift
+++ PicPiTests/PictureFrameTest.swift
@@ -56,20 +56,46 @@ class PictureFrameTest: XCTestCase {
     let image = MediaProvider.init { () -> Promise<(data: Data, filename: String)> in
       return  Promise((imageDataLocal , filenameLocal))
     }
- 
     let expectUpload = expectation(description: "PictureFrame:upload")
     
     frameAPIMock.expectUploadPhotoDataFilename { (imageData, filename) -> Promise<Void> in
+      let promise = Promise<Void>.pending()
+      
       XCTAssertEqual(imageData, imageDataLocal)
       XCTAssertEqual(filename, filenameLocal)
-      expectUpload.fulfill()
-      
-      return Promise(())
+//    promise.reject(FrameAPIErrors.dataMalformed)
+      promise.fulfill(())
+      return promise
     }
-     let pictureFrame = PictureFrameImpl(frameAPI: frameAPIMock, ip: ip)
+    let pictureFrame = PictureFrameImpl(frameAPI: frameAPIMock, ip: ip)
     
-     _ = pictureFrame.storeMedia(images: [image])
+    let session = pictureFrame.storeMedia(images: [image])
     
+    session.didFinishUpload.add(pictureFrame) { (result) in
+      checkResult(
+        result: result.map { result -> (mediaProvider: MediaProvider, filename: String) in
+          (result.payload, result.product)
+        }
+      )
+    }
+    func checkResult(
+      result: Result<(payload: MediaProvider, product: String), Error>
+    ) {
+      switch result {
+      case .success:
+        _ =   result.map { result -> (mediaProvider: MediaProvider, filename: String) in
+          XCTAssertTrue(result.payload === image)
+          XCTAssertEqual(result.product, filenameLocal)
+          ///did not work without expectation "expectUpload"
+          expectUpload.fulfill()
+          
+          return (result.payload , result.product)
+        }
+       case .failure(let error):
+        XCTFail(error.localizedDescription)
+      }
+      
+     }
     waitForExpectations(timeout: 5.0)
   }
 
@@ -83,7 +109,10 @@ class PictureFrameTest: XCTestCase {
     
     let expectPreferences = expectation(description: "PictureFrame:preferences")
     frameAPIMock.expectRetrievePIPrefrences { () -> Promise<PictureFramePreferences> in
-      return Promise(preferencesLocal)
+      let promise = Promise<PictureFramePreferences>.pending()
+//    promise.reject(FrameAPIErrors.dataMalformed)
+      promise.fulfill(preferencesLocal)
+      return promise
     }
     
     let pictureFrame = PictureFrameImpl(frameAPI: frameAPIMock, ip: ip)
@@ -92,13 +121,40 @@ class PictureFrameTest: XCTestCase {
       XCTAssertEqual(preferences.name, preferencesLocal.name)
       XCTAssertEqual(preferences.slideshowDuration, preferencesLocal.slideshowDuration)
       expectPreferences.fulfill()
+    }.catch { (error) in
+      XCTFail(error.localizedDescription)
     }
     
     waitForExpectations(timeout: 5.0)
   }
 
   func testSetPreferences() {
+    let ip = "255.255.255.255"
+    let preferencesLocal = PictureFramePreferences.init(slideshowDuration: 100, connectionPasscode: "", name: "PicPi")
+    
+    let expectPreferences = expectation(description: "PictureFrame:preferences set")
 
+    frameAPIMock.expectUpdatePIPreferences { (preferences) -> Promise<Void> in
+      let promise = Promise<Void>.pending()
+      XCTAssertEqual(preferences.slideshowDuration, preferencesLocal.slideshowDuration)
+      XCTAssertEqual(preferences.connectionPasscode, preferencesLocal.connectionPasscode)
+      XCTAssertEqual(preferences.name, preferencesLocal.name)
+//    promise.reject(FrameAPIErrors.dataMalformed)
+      promise.fulfill(())
+      return promise
+    }
+    let pictureFrame = PictureFrameImpl(frameAPI: frameAPIMock, ip: ip)
+    pictureFrame.set(preferences: preferencesLocal).then { () -> Promise<Void> in
+      let promise = Promise<Void>.pending()
+      promise.fulfill(())
+      expectPreferences.fulfill()
+      return promise
+    }.catch { (error) in
+      XCTFail(error.localizedDescription)
+    }
+   
+    
+    waitForExpectations(timeout: 5.0)
   }
 
   func testForget() {
